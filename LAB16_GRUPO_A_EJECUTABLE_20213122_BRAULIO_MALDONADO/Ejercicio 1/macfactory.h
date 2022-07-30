99e0a45f5228214ec386bd6fe8fa2458e042a24c
Braulio Maldonado Casilla <87953250+ShyniYT@users.noreply.github.com>
2022-07-29 22:11:45 -0500

    Delete LAB16_GRUPO_A_EJECUTABLE_20213122_BRAULIO_MALDONADO/Ejercicio 1 directory

--- LAB16_GRUPO_A_EJECUTABLE_20213122_BRAULIO_MALDONADO/Ejercicio 1/macfactory.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "winfactory.h"
-class MACFACTORY {
-public:
-	virtual ~MACFACTORY() {};
-	virtual std::string Draw() const = 0;
-	virtual std::string draw(const WINFACTORY& colaborador) const = 0;
-};
--- LAB16_GRUPO_A_EJECUTABLE_20213122_BRAULIO_MALDONADO/Ejercicio 1/winfactory.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-class WINFACTORY {
-public:
-	virtual ~WINFACTORY() {};
-	virtual std::string Draw() const = 0;
-};
